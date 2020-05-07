aaa6cff06c2c75890a9431318b71699a41b25690
KennedyNelson <johndoe@example.com>
2020-05-07 12:31:02 +0530

    commit

--- ToDoList/settings.py
+++ ToDoList/settings.py
@@ -126,6 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
@@ -135,3 +137,4 @@ AUTHENTICATION_BACKENDS = (
 )
 
 LOGIN_REDIRECT_URL = 'home'
+
